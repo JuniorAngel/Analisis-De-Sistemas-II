c91d2a857cf495f02da57a0127449d2296ffd973
JuniorAngel <31227538+JuniorAngel@users.noreply.github.com>
2017-08-22 08:43:59 -0600

    cambio 2

--- Pruebas1/Pruebas1/Form1.Designer.cs
+++ Pruebas1/Pruebas1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 331);
+            this.ClientSize = new System.Drawing.Size(646, 402);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
